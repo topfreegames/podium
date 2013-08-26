4d7d0ecbf7d5ddc7266aeb37cbe85b504da48030
Maxwell Da Silva <dayvson@gmail.com>
2013-08-26 18:54:35 -0400

    Adding tests for GetUser, RemoveMember, TotalPages

--- leaderboard_test.go
+++ leaderboard_test.go
@@ -16,18 +16,51 @@ var _ = gocheck.Suite(&S{})
 
 func (s *S) TestRankMember(c *gocheck.C) {
 	highScore := NewLeaderboard("highscore", 10)
-	dayvson, err := highScore.rankMember("dayvson",  481516)
+	dayvson, err := highScore.RankMember("dayvson",  481516)
 	c.Assert(err, gocheck.IsNil)
-	arthur, err := highScore.rankMember("arthur",  1000)
+	arthur, err := highScore.RankMember("arthur",  1000)
 	c.Assert(err, gocheck.IsNil)
-	c.Assert(dayvson.rank, gocheck.DeepEquals, 0)
-	c.Assert(arthur.rank, gocheck.DeepEquals, 1)
+	c.Assert(dayvson.rank, gocheck.Equals, 1)
+	c.Assert(arthur.rank, gocheck.Equals, 2)
 }
 
 func (s *S) TestTotalMembers(c *gocheck.C) {
 	bestTime := NewLeaderboard("bestTime", 10)
 	for i := 0; i<10; i++ {
-		bestTime.rankMember("member_" + strconv.Itoa(i),  1234 * i)	
+		bestTime.RankMember("member_" + strconv.Itoa(i),  1234 * i)	
 	}
-	c.Assert(bestTime.TotalMembers(), gocheck.DeepEquals, 10)
+	c.Assert(bestTime.TotalMembers(), gocheck.Equals, 10)
+}
+
+
+func (s *S) TestRemoveMember(c *gocheck.C) {
+	bestTime := NewLeaderboard("bestTime", 10)
+	for i := 0; i<10; i++ {
+		bestTime.RankMember("member_" + strconv.Itoa(i),  1234 * i)	
+	}
+	c.Assert(bestTime.TotalMembers(), gocheck.Equals, 10)
+	bestTime.RemoveMember("member_5")
+	c.Assert(bestTime.TotalMembers(), gocheck.Equals, 9)
+}
+
+func (s *S) TestTotalPages(c *gocheck.C) {
+	bestTime := NewLeaderboard("All", 25)
+	for i := 0; i<101; i++ {
+		bestTime.RankMember("member_" + strconv.Itoa(i),  1234 * i)	
+	}
+	c.Assert(bestTime.TotalPages(), gocheck.Equals, 5)
+}
+
+func (s *S) TestGetUser( c *gocheck.C) {
+	friendScore := NewLeaderboard("friendScore", 10)
+	dayvson, _ := friendScore.RankMember("dayvson",  12345)
+	felipe, _ := friendScore.RankMember("felipe",  12344)
+	c.Assert(dayvson.rank, gocheck.Equals, 1)
+	c.Assert(felipe.rank, gocheck.Equals, 2)
+	friendScore.RankMember("felipe", 12346)
+	ffelipe, _ := friendScore.GetUser("felipe")
+	fdayvson, _ := friendScore.GetUser("dayvson")
+	c.Assert(ffelipe.rank, gocheck.Equals, 1)
+	c.Assert(fdayvson.rank, gocheck.Equals, 2)
+
 }
