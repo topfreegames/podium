f044c6de6ae9f010556836efefab964d0c8dc810
cscatolini <camila.scatolini@gmail.com>
2016-07-11 17:58:05 -0300

    No need to create several keys.

--- leaderboard/leaderboard_test.go
+++ leaderboard/leaderboard_test.go
@@ -17,55 +17,54 @@ var _ = Describe("Leaderboard", func() {
 		Password: "",
 	}
 
-	BeforeSuite(func() {
+	BeforeEach(func() {
 		conn := util.GetConnection(testRedisSettings)
-		conn.Do("DEL", "highscore")
-		conn.Do("DEL", "bestTime")
-		conn.Do("DEL", "bestWeek")
-		conn.Do("DEL", "friendScore")
-		conn.Do("DEL", "7days")
-		conn.Do("DEL", "bestYear")
-		conn.Do("DEL", "week")
+		conn.Do("DEL", "testleaderbord")
+	})
+
+	AfterSuite(func() {
+		conn := util.GetConnection(testRedisSettings)
+		conn.Do("DEL", "testleaderbord")
 	})
 
 	It("TestSetUserScore", func() {
-		highScore := NewLeaderboard(testRedisSettings, "highscore", 10)
-		dayvson, err := highScore.SetUserScore("dayvson", 481516)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 10)
+		dayvson, err := testLeaderboard.SetUserScore("dayvson", 481516)
 		Expect(err).To(BeNil())
-		arthur, err := highScore.SetUserScore("arthur", 1000)
+		arthur, err := testLeaderboard.SetUserScore("arthur", 1000)
 		Expect(err).To(BeNil())
 		Expect(dayvson.Rank).To(Equal(1))
 		Expect(arthur.Rank).To(Equal(2))
 	})
 
 	It("TestTotalMembers", func() {
-		bestTime := NewLeaderboard(testRedisSettings, "bestTime", 10)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 10)
 		for i := 0; i < 10; i++ {
-			bestTime.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
 		}
-		Expect(bestTime.TotalMembers()).To(Equal(10))
+		Expect(testLeaderboard.TotalMembers()).To(Equal(10))
 	})
 
 	It("TestRemoveMember", func() {
-		bestTime := NewLeaderboard(testRedisSettings, "bestWeek", 10)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 10)
 		for i := 0; i < 10; i++ {
-			bestTime.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
 		}
-		Expect(bestTime.TotalMembers()).To(Equal(10))
-		bestTime.RemoveMember("member_5")
-		Expect(bestTime.TotalMembers()).To(Equal(9))
+		Expect(testLeaderboard.TotalMembers()).To(Equal(10))
+		testLeaderboard.RemoveMember("member_5")
+		Expect(testLeaderboard.TotalMembers()).To(Equal(9))
 	})
 
 	It("TestTotalPages", func() {
-		bestTime := NewLeaderboard(testRedisSettings, "All", 25)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 25)
 		for i := 0; i < 101; i++ {
-			bestTime.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
 		}
-		Expect(bestTime.TotalPages()).To(Equal(5))
+		Expect(testLeaderboard.TotalPages()).To(Equal(5))
 	})
 
 	It("TestGetUser", func() {
-		friendScore := NewLeaderboard(testRedisSettings, "friendScore", 10)
+		friendScore := NewLeaderboard(testRedisSettings, "testleaderbord", 10)
 		dayvson, _ := friendScore.SetUserScore("dayvson", 12345)
 		felipe, _ := friendScore.SetUserScore("felipe", 12344)
 		Expect(dayvson.Rank).To(Equal(1))
@@ -78,37 +77,37 @@ var _ = Describe("Leaderboard", func() {
 	})
 
 	It("TestGetAroundMe", func() {
-		bestTime := NewLeaderboard(testRedisSettings, "BestAllTime", 25)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 25)
 		for i := 0; i < 101; i++ {
-			bestTime.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
 		}
-		users := bestTime.GetAroundMe("member_20")
+		users := testLeaderboard.GetAroundMe("member_20")
 		firstAroundMe := users[0]
-		lastAroundMe := users[bestTime.PageSize-1]
-		Expect(len(users)).To(Equal(bestTime.PageSize))
+		lastAroundMe := users[testLeaderboard.PageSize-1]
+		Expect(len(users)).To(Equal(testLeaderboard.PageSize))
 		Expect(firstAroundMe.PublicID).To(Equal("member_31"))
 		Expect(lastAroundMe.PublicID).To(Equal("member_7"))
 	})
 
 	It("TestGetRank", func() {
-		sevenDays := NewLeaderboard(testRedisSettings, "7days", 25)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 25)
 		for i := 0; i < 101; i++ {
-			sevenDays.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
 		}
-		sevenDays.SetUserScore("member_6", 1000)
-		Expect(sevenDays.GetRank("member_6")).To(Equal(100))
+		testLeaderboard.SetUserScore("member_6", 1000)
+		Expect(testLeaderboard.GetRank("member_6")).To(Equal(100))
 	})
 
 	It("TestGetLeaders", func() {
-		bestYear := NewLeaderboard(testRedisSettings, "bestYear", 25)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 25)
 		for i := 0; i < 1000; i++ {
-			bestYear.SetUserScore("member_"+strconv.Itoa(i+1), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i+1), 1234*i)
 		}
-		var users = bestYear.GetLeaders(1)
+		var users = testLeaderboard.GetLeaders(1)
 
 		firstOnPage := users[0]
 		lastOnPage := users[len(users)-1]
-		Expect(len(users)).To(Equal(bestYear.PageSize))
+		Expect(len(users)).To(Equal(testLeaderboard.PageSize))
 		Expect(firstOnPage.PublicID).To(Equal("member_1000"))
 		Expect(firstOnPage.Rank).To(Equal(1))
 		Expect(lastOnPage.PublicID).To(Equal("member_976"))
@@ -116,11 +115,11 @@ var _ = Describe("Leaderboard", func() {
 	})
 
 	It("TestGetUserByRank", func() {
-		sevenDays := NewLeaderboard(testRedisSettings, "week", 25)
+		testLeaderboard := NewLeaderboard(testRedisSettings, "testleaderbord", 25)
 		for i := 0; i < 101; i++ {
-			sevenDays.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
+			testLeaderboard.SetUserScore("member_"+strconv.Itoa(i), 1234*i)
 		}
-		member := sevenDays.GetMemberByRank(10)
+		member := testLeaderboard.GetMemberByRank(10)
 		Expect(member.PublicID).To(Equal("member_91"))
 		Expect(member.Rank).To(Equal(10))
 	})
