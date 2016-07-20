ab9c941bc7fee5917df33133064e72c7399b0563
Bernardo Heynemann <heynemann@gmail.com>
2016-07-20 19:53:59 -0300

    Pushing docker image

---
+++ scripts/get_latest_tag.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Podium
+# https://github.com/topfreegames/podium
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright © 2016 Top Free Games <backend@tfgco.com>
+
+import urllib
+import urllib2
+import json
+
+def main():
+    url = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:tfgco/podium:pull,push"
+    response = urllib.urlopen(url)
+    token = json.loads(response.read())['token']
+
+    url = "https://registry-1.docker.io/v2/tfgco/podium/tags/list"
+    req = urllib2.Request(url, None, {
+        "Authorization": "Bearer %s" % token,
+    })
+    response = urllib2.urlopen(req)
+    tags = json.loads(response.read())
+    last_tag = get_last_tag(tags['tags'])
+    print last_tag
+
+
+def get_tag_value(tag):
+    if tag[0] == "latest":
+        return 0
+
+    while len(tag) < 4:
+        tag.append('0')
+
+    total_value = 0
+    for index, tag_part in enumerate(tag):
+        power = pow(100, len(tag) - index)
+        total_value += int(tag_part) * power
+
+    return total_value
+
+
+def get_last_tag(tags):
+    return '.'.join(
+        max([
+            (get_tag_value(tag), tag) for tag in
+                [t.split('.') for t in tags]
+            ], key=lambda i: i[0]
+        )[1]
+    )
+
+
+if __name__ == "__main__":
+    main()
--- scripts/push_to_docker.sh
+++ scripts/push_to_docker.sh
@@ -5,7 +5,7 @@ VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed
 cp ./config/default.yaml ./dev
 
 docker build -t podium .
-docker build -t podium-dev -f ./DevDockerfile .
+#docker build -t podium-dev -f ./DevDockerfile .
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 docker tag podium:latest tfgco/podium:$VERSION.$TRAVIS_BUILD_NUMBER
@@ -13,10 +13,10 @@ docker push tfgco/podium:$VERSION.$TRAVIS_BUILD_NUMBER
 docker tag podium:latest tfgco/podium:latest
 docker push tfgco/podium
 
-docker tag podium-dev:latest tfgco/podium-dev:$VERSION.$TRAVIS_BUILD_NUMBER
-docker push tfgco/podium-dev:$VERSION.$TRAVIS_BUILD_NUMBER
-docker tag podium-dev:latest tfgco/podium-dev:latest
-docker push tfgco/podium-dev
+#docker tag podium-dev:latest tfgco/podium-dev:$VERSION.$TRAVIS_BUILD_NUMBER
+#docker push tfgco/podium-dev:$VERSION.$TRAVIS_BUILD_NUMBER
+#docker tag podium-dev:latest tfgco/podium-dev:latest
+#docker push tfgco/podium-dev
 
 DOCKERHUB_LATEST=$(python ./scripts/get_latest_tag.py)
 
