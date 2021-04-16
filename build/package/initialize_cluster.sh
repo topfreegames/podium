201525bb3900a976ae82b98b72a5c7b580eae109
Arthur Nogueira Gonçalves <arthur.nogueira@wildlifestudios.com>
2021-04-16 11:45:21 -0300

    Remove unused i variable

--- build/package/initialize_cluster.sh
+++ build/package/initialize_cluster.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 HOSTS=$(echo $CLUSTER_HOSTS | tr " " "\n")
-i=0
 HOST_PORTS=()
 for element in $HOSTS; do
   HOST_NAME=$(echo $element | cut -d ":" -f 1)
@@ -9,7 +8,6 @@ for element in $HOSTS; do
   IP=$(host ${HOST_NAME} | grep "has address" | cut -d " " -f 4)
 
   HOST_PORTS=("${HOST_PORTS[@]}" "$(echo $IP:$PORT)")
-  i=$(($i + 1))
 done
 
 redis-cli --cluster create ${HOST_PORTS[@]} --cluster-yes
