25932f0791efad57d629cc75014ebf457b613d48
Arthur Nogueira Gonçalves <arthur.nogueira@wildlifestudios.com>
2021-04-28 19:35:11 -0300

    Create GetAroundMe service

--- leaderboard/service/get_around_me.go
+++ leaderboard/service/get_around_me.go
@@ -3,9 +3,41 @@ package service
 import (
 	"context"
 
+	"github.com/topfreegames/podium/leaderboard/database"
 	"github.com/topfreegames/podium/leaderboard/model"
 )
 
+const getAroundMeServiceLabel = "get around me"
+
+// GetAroundMe find users around a certain member
 func (s *Service) GetAroundMe(ctx context.Context, leaderboard string, pageSize int, member string, order string, getLastIfNotFound bool) ([]*model.Member, error) {
-	return nil, nil
+	memberRank, err := s.Database.GetRank(ctx, leaderboard, member, order)
+	if err != nil {
+		if _, ok := err.(*database.MemberNotFoundError); ok {
+			if !getLastIfNotFound {
+				return nil, NewMemberNotFoundError(leaderboard, member)
+			}
+
+			memberRank, err = s.Database.GetTotalMembers(ctx, leaderboard)
+			if err != nil {
+				return nil, NewGeneralError(getAroundMeServiceLabel, err.Error())
+			}
+		} else {
+			return nil, NewGeneralError(getAroundMeServiceLabel, err.Error())
+		}
+	}
+
+	indexes, err := s.calculateIndexesAroundMemberRank(ctx, leaderboard, memberRank, pageSize)
+	if err != nil {
+		return nil, NewGeneralError(getAroundMeServiceLabel, err.Error())
+	}
+
+	databaseMembers, err := s.Database.GetOrderedMembers(ctx, leaderboard, indexes.Start, indexes.Stop, order)
+	if err != nil {
+		return nil, NewGeneralError(getAroundMeServiceLabel, err.Error())
+	}
+
+	members := convertDatabaseMembersIntoModelMembers(databaseMembers)
+
+	return members, nil
 }
