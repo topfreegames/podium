ab89a082ed8e8163c90947a538c32200087c1fdc
Arthur Nogueira Gonçalves <arthur.nogueira@wildlifestudios.com>
2021-05-03 19:38:24 -0300

    Add set members score service

--- leaderboard/service/set_members_score.go
+++ leaderboard/service/set_members_score.go
@@ -12,27 +12,34 @@ import (
 
 const setMembersScoreServiceLabel = "set members score"
 
+const setMembersOrder = "desc"
+
 // SetMembersScore return member informations that is
 func (s *Service) SetMembersScore(ctx context.Context, leaderboard string, members []*model.Member, prevRank bool, scoreTTL string) error {
-	err := s.setLeaderboardExpirationTime(ctx, leaderboard)
-	if err != nil {
-		return err
-	}
-
 	if prevRank {
-		err := s.setMembersPreviousRank(ctx, leaderboard, members, "desc")
+		err := s.setMembersPreviousRank(ctx, leaderboard, members, setMembersOrder)
 		if err != nil {
 			return err
 		}
 	}
 
-	err = s.setMembers(ctx, leaderboard, members)
+	err := s.persistMembers(ctx, leaderboard, members)
+	if err != nil {
+		return err
+	}
+
+	err = s.setMembersRank(ctx, leaderboard, members, setMembersOrder)
+	if err != nil {
+		return err
+	}
+
+	err = s.persistLeaderboardExpirationTime(ctx, leaderboard)
 	if err != nil {
 		return err
 	}
 
 	if scoreTTL != "" {
-		err = s.setMembersTTL(ctx, leaderboard, members, scoreTTL)
+		err = s.persistMembersTTL(ctx, leaderboard, members, scoreTTL)
 		if err != nil {
 			return err
 		}
@@ -41,7 +48,66 @@ func (s *Service) SetMembersScore(ctx context.Context, leaderboard string, membe
 	return nil
 }
 
-func (s *Service) setLeaderboardExpirationTime(ctx context.Context, leaderboard string) error {
+func (s *Service) setMembersPreviousRank(ctx context.Context, leaderboard string, members []*model.Member, order string) error {
+	databaseMembers, err := s.getDatabaseMembers(ctx, leaderboard, members, order)
+	if err != nil {
+		return err
+	}
+
+	for i, member := range members {
+		if databaseMembers[i] != nil {
+			member.PreviousRank = int(databaseMembers[i].Rank + 1)
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) persistMembers(ctx context.Context, leaderboard string, members []*model.Member) error {
+	databaseMembers := make([]*database.Member, 0, len(members))
+	for _, member := range members {
+		databaseMembers = append(databaseMembers, &database.Member{
+			Member: member.PublicID,
+			Score:  float64(member.Score),
+		})
+	}
+
+	err := s.Database.SetMembers(ctx, leaderboard, databaseMembers)
+	if err != nil {
+		return NewGeneralError(setMembersScoreServiceLabel, err.Error())
+	}
+
+	return nil
+}
+
+func (s *Service) setMembersRank(ctx context.Context, leaderboard string, members []*model.Member, order string) error {
+	databaseMembers, err := s.getDatabaseMembers(ctx, leaderboard, members, order)
+	if err != nil {
+		return NewGeneralError(setMembersScoreServiceLabel, err.Error())
+	}
+
+	for i, member := range databaseMembers {
+		members[i].Rank = int(member.Rank + 1)
+	}
+
+	return nil
+}
+
+func (s *Service) getDatabaseMembers(ctx context.Context, leaderboard string, members []*model.Member, order string) ([]*database.Member, error) {
+	memberIDs := make([]string, 0, len(members))
+	for _, member := range members {
+		memberIDs = append(memberIDs, member.PublicID)
+	}
+
+	databaseMembers, err := s.Database.GetMembers(ctx, leaderboard, order, true, memberIDs...)
+	if err != nil {
+		return nil, NewGeneralError(setMembersScoreServiceLabel, err.Error())
+	}
+
+	return databaseMembers, nil
+}
+
+func (s *Service) persistLeaderboardExpirationTime(ctx context.Context, leaderboard string) error {
 	expireAt, err := expiration.GetExpireAt(leaderboard)
 	if err != nil {
 		return NewGeneralError(setMembersScoreServiceLabel, err.Error())
@@ -63,47 +129,10 @@ func (s *Service) setLeaderboardExpirationTime(ctx context.Context, leaderboard 
 	return nil
 }
 
-func (s *Service) setMembersPreviousRank(ctx context.Context, leaderboard string, members []*model.Member, order string) error {
-	memberIDs := make([]string, 0, len(members))
-	for _, member := range members {
-		memberIDs = append(memberIDs, member.PublicID)
-	}
-
-	databaseMembers, err := s.Database.GetMembers(ctx, leaderboard, order, true, memberIDs...)
-	if err != nil {
-		return NewGeneralError(getMembersServiceLabel, err.Error())
-	}
-
-	for i, member := range members {
-		if databaseMembers[i] != nil {
-			member.PreviousRank = int(databaseMembers[i].Rank)
-		}
-	}
-
-	return nil
-}
-
-func (s *Service) setMembers(ctx context.Context, leaderboard string, members []*model.Member) error {
-	databaseMembers := make([]*database.Member, 0, len(members))
-	for _, member := range members {
-		databaseMembers = append(databaseMembers, &database.Member{
-			Member: member.PublicID,
-			Score:  float64(member.Score),
-		})
-	}
-
-	err := s.Database.SetMembers(ctx, leaderboard, databaseMembers)
-	if err != nil {
-		return NewGeneralError(setMembersScoreServiceLabel, err.Error())
-	}
-
-	return nil
-}
-
-func (s *Service) setMembersTTL(ctx context.Context, leaderboard string, members []*model.Member, scoreTTL string) error {
+func (s *Service) persistMembersTTL(ctx context.Context, leaderboard string, members []*model.Member, scoreTTL string) error {
 	ttl, err := strconv.ParseInt(scoreTTL, 10, 64)
 	if err != nil {
-		return NewGeneralError(getMembersServiceLabel, err.Error())
+		return NewGeneralError(setMembersScoreServiceLabel, err.Error())
 	}
 
 	timeToExpire := time.Now().UTC().Add(time.Duration(ttl) * time.Second)
